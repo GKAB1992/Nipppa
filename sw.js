c8ab0d2dbf621584484b2c7414af3d78cd72ac21
GKAB1992 <163327104+GKAB1992@users.noreply.github.com>
2024-03-13 18:13:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,21 @@
-const CACHE_NAME = `Nipper`;
 
-// Use the install event to pre-cache all initial resources.
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/',
-      '/https://cdn.jsdelivr.net/npm/chart.js',
-      '/https://unpkg.com/simple-statistics@7.8.2/dist/simple-statistics.min.js'
-    ]);
-  })());
-});
+// Change this to your repository name
+var GHPATH = '/Nipppa';
 
-self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
+// Choose a different app prefix name
+var APP_PREFIX = 'gppwa_';
 
-    // Get the resource from the cache.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
 
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
-        }
-    }
-  })());
-});
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/styles.css`,
+  `${GHPATH}/js/app.js`
+]
